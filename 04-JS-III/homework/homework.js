e472ce308ae8e485cfc910211731e9d5f214b7f1
AlejoMl1 <a.munoz254@uniandes.edu.co>
2021-07-14 18:19:32 -0500

    Homework 4 & 5.

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,43 +3,57 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un array
   // Tu código:
+  return array[0];
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array[array.length-1];
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length;
 }
 
-
+//console.log(obtenerLargoDelArray([1,2,3]))
 function incrementarPorUno(array) {
   // "array" debe ser una matriz de enteros (int/integers)
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  var an = [];
+  for (var i=0; i< array.length;i++){
+    an[i] = array[i]+1;
+    console.log(an[i]);
+  }
+  return an;
 }
 
+//console.log( incrementarPorUno([1,2,3,4]))
 
 function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento)
+  return array;
 }
-
+//console.log(agregarItemAlFinalDelArray([1,2],3))
 
 function agregarItemAlComienzoDelArray(array, elemento) {
   // Añade el "elemento" al comienzo del array
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento);
+  return array;
 }
-
+//console.log(agregarItemAlComienzoDelArray([1,2,3],10))
 
 function dePalabrasAFrase(palabras) {
   // "palabras" es un array de strings/cadenas
@@ -47,20 +61,35 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  var newWord= '';
+  for (var i=0;i< palabras.length;i++){
+    
+    newWord = newWord + palabras[i]+ " ";
+  }
+  return newWord;
 }
-
-
+//console.log(dePalabrasAFrase(['hola','pepe']))
 function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  var existe = false;
+  for ( var i=0; i< array.length;i++){
+    if ( array[i]=== elemento){
+      existe = true;
+    }
+  }
+  return existe;
 }
-
-
+//console.log( arrayContiene( [ 'a','b','c'],'d') )
 function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  suma=0;
+
+
+  
 }
 
 
